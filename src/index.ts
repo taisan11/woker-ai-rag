ede6a05393ce9b41ef7da9045b8d67ca1287620f
taisan <131413638+taisan11@users.noreply.github.com>
2024-07-04 14:11:01 +0900

    end

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', async (c) => {
+  const answer = await c.env.AI.run(
+    '@cf/meta/llama-3-8b-instruct',
+    {
+      messages: [
+        { role: 'user', content: `こんにちは` }
+      ]
+    }
+  )
+
+  return c.html(answer.response)
 })
 
 export default app
